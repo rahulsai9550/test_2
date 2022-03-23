2167d171f3856e7a1dea2c2394fd98552773e2b1
Rahul sai <rahulsai5808@gmail.com>
2022-03-23 16:33:54 +0530

    Asking user whether it is first commit

--- FirstGitFile.java
+++ FirstGitFile.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 class FirstGitFile{
   public static void main(String[] args){
-    System.out.println("This is Rahul's first java file in git");
+    Scanner sc = new Scanner(System.in);
+    String s = sc.nextLine();
+    if(s.equals("first")){
+      System.out.println("This is the first committed file");
+    }
+    else{
+      System.out.println("Not the first committed file");
+    }
   }
 }
